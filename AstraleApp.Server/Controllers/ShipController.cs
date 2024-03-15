6a4df1f158514e3ff8688a93e6aa31a9e13f8ec2
David <daniel.david.surla@gmail.com>
2024-03-15 15:09:42 +1100

    Needed decorators to actually be logged and work...am silly my dude.

--- AstraleApp.Server/Controllers/ShipController.cs
+++ AstraleApp.Server/Controllers/ShipController.cs
@@ -3,19 +3,31 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AstraleApp.Server.Controllers
 {
+  [ApiController]
+  [Route("api/[controller]")]
   public class ShipController : Controller
   {
     private readonly Ship _ship;
 
-    public ShipController(Ship ship)
+    private readonly string[] _attributes = new[]
     {
+      "Commander", "Capabilities", "Training", "Speed", "Endurance",
+      "Dimensions", "Propulsion", "Navigation", "Communications",
+      "Weapons", "Accomodation", "Equipment", "Boats"
+    };
+
+    private readonly ILogger<ShipController> _logger;
+
+    public ShipController(ILogger<ShipController> logger, Ship ship)
+    {
+      this._logger = logger;
       this._ship = ship;
     }
 
-    [HttpGet(Name = "GetShip")]
-    public string GetShip()
+    [HttpGet]
+    public string[] GetAttributes()
     {
-      return "ship";
+      return this._attributes;
     }
 
     [HttpPut(Name = "UpdateShip")]
@@ -35,5 +47,5 @@ namespace AstraleApp.Server.Controllers
     {
     }
   }
-  }
 }
+
