1fd688f7e061931de2022d202e68e12eb29fdb9a
David <daniel.david.surla@gmail.com>
2024-09-25 08:28:51 +1000

    Update tailwind.config.js

--- tourteam.client/tailwind.config.js
+++ tourteam.client/tailwind.config.js
@@ -1,6 +1,13 @@
 /** @type {import("tailwindcss").Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+  colors: {
+    "text": "#110b18",
+    "background": "#fbfafd",
+    "primary": "#e0dce5",
+    "secondary": "#ffa07a",
+    "accent": "#c3c482",
+  },
   theme: {
     extend: {}
   },
